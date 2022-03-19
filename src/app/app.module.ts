9f514efcee4df711ed492c73cbe80a04643ae895
Eduardo Vales <eduardo.vales@gmail.com>
2022-03-19 18:48:35 -0300

    Create - Variáveis SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-//Modules
+//ModulesAplicativo com listas de todas as tarefas que você precisa realizar, ou seja, elas listam tudo que você tem a fazer, com as tarefas mais importantes no topo, e as tarefas menos importantes na parte inferior da lista
 import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
